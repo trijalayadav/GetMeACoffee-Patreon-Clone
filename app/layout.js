8909d7a6dd887c9996e5b07d4fe2c0c2cba1c709
trijala-product-manager <trijala1234@gmail.com>
2025-04-12 21:38:55 +0530

    added github authorization

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import SessionWrapper from "@/components/SessionWrapper";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,15 +23,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Navbar />
-        <div className="relative">
-          <div className="min-h-[81vh] w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
-            {children}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <SessionWrapper>
+          <Navbar />
+          <div className="relative">
+            <div className="min-h-[81vh] w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
+              {children}
+            </div>
           </div>
-        </div>
-        <Footer />
+          <Footer />
+        </SessionWrapper>
 
       </body >
     </html >
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,16 @@
+"use client"
 import React from 'react'
 import Link from 'next/link'
+import { useSession, signIn, signOut } from "next-auth/react"
 
 const Navbar = () => {
+    const { data: session } = useSession()
+    if (session) {
+        return <>
+            Signed in as {session.user.email} <br />
+            <button onClick={() => signOut()}>Sign out</button>
+        </>
+    }
     return (
         <nav className='bg-black text-white flex justify-between items-center px-4 h-14 py-2'>
             <div className='logo font-bold flex flex-row items-center gap-3'>
@@ -17,7 +26,7 @@ const Navbar = () => {
             </ul> */}
             <div>
                 <Link href={"/login"}>
-                    <button type="button" className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Login</button>
+                    <button type="button" className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" >Login</button>
                 </Link>
             </div>
         </nav>
